5db96a697f461a42592ac3c7fd83185889f2b568
maria <maria@LAPTOP-054VRH9O>
2020-03-26 05:01:44 +0100

    Commit inicial 2

--- entornos46/src/entornos46/Entornos46.java
+++ entornos46/src/entornos46/Entornos46.java
@@ -5,17 +5,94 @@
  */
 package entornos46;
 
+import java.util.Scanner;
+
 /**
  *
  * @author maria
  */
 public class Entornos46 {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static Scanner sc = new Scanner(System.in);
+    
+    static char[] letras;
+
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        System.out.println("------- Ejercicio cadena caracteres Array -------");
+
+        System.out.println("Introduce una frase: ");
+        String frase = sc.nextLine();
+        String cadena = frase.toLowerCase();//frase pero en minúsculas.
+
+        System.out.println(textoCaracteresTotal(contarCaracteresTotal(cadena)));
+        System.out.println(textoNumeroPalabras(contarNumeroPalabras(cadena)));
+        System.out.println(textoTodosCaracteres(contarTodosCaracteres(cadena)));
+        
+    }
+    
+    static int contarCaracteresTotal(String cadena){
+        //devuelve el número total de caracteres dada una determinada cadena
+        int cantidadCaracteres = 0;
+        
+        for (int i = 0; i < cadena.length(); i++) {
+            if (cadena.charAt(i) != ' ') {
+                cantidadCaracteres++;
+            }
+        }
+        return cantidadCaracteres;
+    }
+    
+    static int contarNumeroPalabras(String cadena){
+        //cuenta el número de palabras introducido
+        int cantidadPalabras = 1;
+        
+        for (int i = 0; i < cadena.length(); i++) {
+            if (cadena.charAt(i) == ' ' && cadena.charAt(i - 1) != ' ') {
+                cantidadPalabras++;
+            }
+        }
+        return cantidadPalabras;
+        
+    }
+    
+    static int[] contarTodosCaracteres(String cadena){
+        //devuelve la cantidad de caracteres de cada tipo
+        letras = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's',
+            't', 'u', 'v', 'w', 'x', 'y', 'z', '.', ',', '?',
+            '¿', '!', '¡', ';', ':', '(', ')'};
+        int[] contador = new int[37];
+        for (int i = 0; i < cadena.length(); i++) {
+
+            for (int j = 0; j < contador.length; j++) {
+                if (cadena.charAt(i) == letras[j]) {
+                    contador[j]++;
+                }
+            }
+        }
+        return contador;
+        
+    }
+    
+    static String textoCaracteresTotal (int total){
+        
+        return "Cantidad de caracteres: " + total;
+    }
+    
+    static String textoNumeroPalabras (int total){
+        
+        return "Cantidad de palabras: " + total;
+    }
+    
+    static String textoTodosCaracteres (int[] contador){
+        String resultado = "";
+        for (int i = 0; i < contador.length; i++) {
+            if (contador[i] > 0 && letras[i] != ' ') {
+                resultado += "Cantidad de " + letras[i] + ": " + contador[i] + "\n";
+            }
+        }
+        return resultado;
     }
     
 }
