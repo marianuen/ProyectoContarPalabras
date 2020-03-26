c78db3d4e47e31060e6555722694419631362087
maria <maria@LAPTOP-054VRH9O>
2020-03-26 05:08:42 +0100

    Clase calculos creada

--- entornos46/src/entornos46/Entornos46.java
+++ entornos46/src/entornos46/Entornos46.java
@@ -25,55 +25,13 @@ public class Entornos46 {
         String frase = sc.nextLine();
         String cadena = frase.toLowerCase();//frase pero en minúsculas.
 
-        System.out.println(textoCaracteresTotal(contarCaracteresTotal(cadena)));
-        System.out.println(textoNumeroPalabras(contarNumeroPalabras(cadena)));
-        System.out.println(textoTodosCaracteres(contarTodosCaracteres(cadena)));
+        System.out.println(textoCaracteresTotal(Calculos.contarCaracteresTotal(cadena)));
+        System.out.println(textoNumeroPalabras(Calculos.contarNumeroPalabras(cadena)));
+        System.out.println(textoTodosCaracteres(Calculos.contarTodosCaracteres(cadena)));
         
     }
     
-    static int contarCaracteresTotal(String cadena){
-        //devuelve el número total de caracteres dada una determinada cadena
-        int cantidadCaracteres = 0;
-        
-        for (int i = 0; i < cadena.length(); i++) {
-            if (cadena.charAt(i) != ' ') {
-                cantidadCaracteres++;
-            }
-        }
-        return cantidadCaracteres;
-    }
     
-    static int contarNumeroPalabras(String cadena){
-        //cuenta el número de palabras introducido
-        int cantidadPalabras = 1;
-        
-        for (int i = 0; i < cadena.length(); i++) {
-            if (cadena.charAt(i) == ' ' && cadena.charAt(i - 1) != ' ') {
-                cantidadPalabras++;
-            }
-        }
-        return cantidadPalabras;
-        
-    }
-    
-    static int[] contarTodosCaracteres(String cadena){
-        //devuelve la cantidad de caracteres de cada tipo
-        letras = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-            'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's',
-            't', 'u', 'v', 'w', 'x', 'y', 'z', '.', ',', '?',
-            '¿', '!', '¡', ';', ':', '(', ')'};
-        int[] contador = new int[37];
-        for (int i = 0; i < cadena.length(); i++) {
-
-            for (int j = 0; j < contador.length; j++) {
-                if (cadena.charAt(i) == letras[j]) {
-                    contador[j]++;
-                }
-            }
-        }
-        return contador;
-        
-    }
     
     static String textoCaracteresTotal (int total){
         
