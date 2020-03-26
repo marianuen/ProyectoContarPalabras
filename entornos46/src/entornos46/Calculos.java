e414044664abf2c4f222c4b790796ab39806b6c9
maria <maria@LAPTOP-054VRH9O>
2020-03-26 10:00:54 +0100

    La clase "Calculos" se ha documentado con javadoc.

--- entornos46/src/entornos46/Calculos.java
+++ entornos46/src/entornos46/Calculos.java
@@ -4,11 +4,20 @@ import static entornos46.Entornos46.letras;
 
 
 /**
- *
+ * Se encarga exclusivamente de realizar diferentes conteos con la frase
+ * dada por el usuario
+ * 
  * @author maria
  */
 public class Calculos {
     
+    /**
+     * Cuenta los caracteres que tiene la frase
+     * 
+     * @param cadena es la frase que insertará el usuario
+     * @return la cantidad total de caracteres que tiene la frase dada por el 
+     * usuario
+     */
     public static int contarCaracteresTotal(String cadena){
         //devuelve el número total de caracteres dada una determinada cadena
         int cantidadCaracteres = 0;
@@ -21,6 +30,12 @@ public class Calculos {
         return cantidadCaracteres;
     }
     
+    /**
+     * Cuenta el número de palabras que tiene la frase
+     * 
+     * @param cadena es la frase que insertará el usuario
+     * @return la cantidad de palabras que tiene  la frase dada por el usuario
+     */
     public static int contarNumeroPalabras(String cadena){
         //cuenta el número de palabras introducido
         int cantidadPalabras = 1;
@@ -34,6 +49,13 @@ public class Calculos {
         
     }
     
+    /**
+     * Cuenta las veces que se repite cada letra en la frase
+     * 
+     * @param cadena es la frase que insertará el usuario
+     * @return un contador, que contiene el numero de veces que aparece una letra
+     * (o símbolo) en la frase que ha insertado el usuario. 
+     */
     public static int[] contarTodosCaracteres(String cadena){
         //devuelve la cantidad de caracteres de cada tipo
         letras = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
