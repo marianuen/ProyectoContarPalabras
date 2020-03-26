eefad4ab10b97b8f7c2df55018c287d8cbc80272
maria <maria@LAPTOP-054VRH9O>
2020-03-26 10:07:28 +0100

    La clase "Entornos46" (main)  se ha documentado con javadoc.

--- entornos46/src/entornos46/Entornos46.java
+++ entornos46/src/entornos46/Entornos46.java
@@ -8,15 +8,26 @@ package entornos46;
 import java.util.Scanner;
 
 /**
- *
+ * Es la clase principal. En este proyecto se pide una frase al usuario, y se
+ * realizan diferentes conteos con esa misma frase (número de caracteres, número 
+ * de palabras y cantidad de veces que se repite cada letra o símbolo)
+ * 
  * @author maria
  */
 public class Entornos46 {
 
+    /**
+     * Crea un objeto de la clase Scanner, que permite pedir datos al usuario.
+     */
     public static Scanner sc = new Scanner(System.in);
     
     static char[] letras;
 
+    /**
+     * Contiene el main. Se encarga de pedir una frase al usuario, y de ejecutar
+     * los diferentes métodos de las diferentes clases que contiene el proyecto.
+     * @param args
+     */
     public static void main(String[] args) {
 
         System.out.println("------- Ejercicio cadena caracteres Array -------");
